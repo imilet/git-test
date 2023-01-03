5355b6a3d98a285cc9e238fb29de7b9fb71cb257
lvan <lvanio@163.com>
2023-01-03 15:35:13 +0800

    first commit

---
+++ src/main/java/com/lyp/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.lyp.git;
+
+/**
+ * Date：2023/1/3
+ * Author：lyp
+ * Description：
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
